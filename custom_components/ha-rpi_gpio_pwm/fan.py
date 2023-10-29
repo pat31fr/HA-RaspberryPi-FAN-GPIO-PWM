5938f266f79078617e09dfcecaa9a840e6a497b7
Github GPG acces <erdnaxela02@hotmail.fr>
2023-10-29 22:48:46 +0100

    Convert Light to Fan

--- custom_components/ha-rpi_gpio_pwm/fan.py
+++ custom_components/ha-rpi_gpio_pwm/fan.py
@@ -1,20 +1,18 @@
-"""Support for LED lights that can be controlled using PWM."""
+"""Support for Fan that can be controlled using PWM."""
 from __future__ import annotations
 
 import logging
 
-from gpiozero import PWMLED
+from gpiozero import PWMOutputDevice
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 import voluptuous as vol
 
-from homeassistant.components.light import (
-    ATTR_BRIGHTNESS,
-    ATTR_TRANSITION,
+from homeassistant.components.fan import (
+    ATTR_PERCENTAGE,
     PLATFORM_SCHEMA,
-    SUPPORT_BRIGHTNESS,
-    SUPPORT_TRANSITION,
-    LightEntity,
+    SUPPORT_SET_SPEED,
+    FanEntity,
 )
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME, STATE_ON
 from homeassistant.core import HomeAssistant
@@ -25,26 +23,24 @@ from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_LEDS = "leds"
+CONF_FANS = "fans"
 CONF_PIN = "pin"
-CONF_FREQUENCY = "frequency"
 
-DEFAULT_BRIGHTNESS = 255
+DEFAULT_PERCENTAGE = 100
 
 DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 8888
 
-SUPPORT_SIMPLE_LED = SUPPORT_BRIGHTNESS | SUPPORT_TRANSITION
+SUPPORT_SIMPLE_FAN = SUPPORT_SET_SPEED
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_LEDS): vol.All(
+        vol.Required(CONF_FANS): vol.All(
             cv.ensure_list,
             [
                 {
                     vol.Required(CONF_NAME): cv.string,
                     vol.Required(CONF_PIN): cv.positive_int,
-                    vol.Optional(CONF_FREQUENCY): cv.positive_int,
                     vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
                     vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
                 }
@@ -60,37 +56,35 @@ def setup_platform(
     add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
-    """Set up the PWM LED lights."""
-    leds = []
-    for led_conf in config[CONF_LEDS]:
-        pin = led_conf[CONF_PIN]
+    """Set up the PWM FAN."""
+    fans = []
+    for fan_conf in config[CONF_FANS]:
+        pin = fan_conf[CONF_PIN]
         opt_args = {}
-        if CONF_FREQUENCY in led_conf:
-            opt_args["frequency"] = led_conf[CONF_FREQUENCY]
-        opt_args["pin_factory"] = PiGPIOFactory(host=led_conf[CONF_HOST], port= led_conf[CONF_PORT])
-        led = PwmSimpleLed(PWMLED(pin, **opt_args), led_conf[CONF_NAME])
-        leds.append(led)
+        opt_args["pin_factory"] = PiGPIOFactory(host=fan_conf[CONF_HOST], port= fan_conf[CONF_PORT])
+        fan = PwmSimpleFan(PWMOutputDevice(pin, **opt_args), fan_conf[CONF_NAME])
+        fans.append(fan)
 
-    add_entities(leds)
+    add_entities(fans)
 
 
-class PwmSimpleLed(LightEntity, RestoreEntity):
-    """Representation of a simple one-color PWM LED."""
+class PwmSimpleFan(FanEntity, RestoreEntity):
+    """Representation of a simple PWM FAN."""
 
-    def __init__(self, led, name):
-        """Initialize one-color PWM LED."""
-        self._led = led
+    def __init__(self, fan, name):
+        """Initialize PWM FAN."""
+        self._fan = fan
         self._name = name
         self._is_on = False
-        self._brightness = DEFAULT_BRIGHTNESS
+        self._percentage = DEFAULT_PERCENTAGE
 
     async def async_added_to_hass(self):
         """Handle entity about to be added to hass event."""
         await super().async_added_to_hass()
         if last_state := await self.async_get_last_state():
             self._is_on = last_state.state == STATE_ON
-            self._brightness = last_state.attributes.get(
-                "brightness", DEFAULT_BRIGHTNESS
+            self._percentage = last_state.attributes.get(
+                "percentage", DEFAULT_PERCENTAGE
             )
 
     @property
@@ -109,30 +103,33 @@ class PwmSimpleLed(LightEntity, RestoreEntity):
         return self._is_on
 
     @property
-    def brightness(self):
-        """Return the brightness property."""
-        return self._brightness
+    def percentage(self):
+        """Return the percentage property."""
+        return self._percentage
 
     @property
     def supported_features(self):
         """Flag supported features."""
-        return SUPPORT_SIMPLE_LED
+        return SUPPORT_SIMPLE_FAN
 
-    def turn_on(self, **kwargs):
-        """Turn on a led."""
-        if ATTR_BRIGHTNESS in kwargs:
-            self._brightness = kwargs[ATTR_BRIGHTNESS]
-        self._led.value = _from_hass_brightness(self._brightness)
+    def turn_on(self, percentage: Optional[int] = None, preset_mode: Optional[str] = None, **kwargs: Any) -> None:
+        """Turn on the fan."""
+        if ATTR_PERCENTAGE in kwargs:
+            self._percentage = kwargs[ATTR_PERCENTAGE]
+        self._fan.value = self._percentage / 100
         self._is_on = True
         self.schedule_update_ha_state()
 
-    def turn_off(self, **kwargs):
-        """Turn off a LED."""
+    def turn_off(self, **kwargs: Any) -> None:
+        """Turn the fan off."""
         if self.is_on:
-            self._led.off()
+            self._fan.off()
         self._is_on = False
         self.schedule_update_ha_state()
 
-def _from_hass_brightness(brightness):
-    """Convert Home Assistant brightness units to percentage."""
-    return brightness / 255
+    def set_percentage(self, percentage: int) -> None:
+        """Set the speed percentage of the fan."""
+        self._percentage = percentage
+        self._fan.value = self._percentage / 100
+        self._is_on = True
+        self.schedule_update_ha_state()
--- info.md
+++ info.md
@@ -1,43 +1,76 @@
-# Home Assistant Raspberry Pi GPIO PWM custom integration
+# Home Assistant Raspberry Pi Fan GPIO PWM custom integration
+
+## About
+
+This project is a fork of the original [RedMeKool](https://github.com/RedMeKool/HA-Raspberry-pi-GPIO-PWM) project which I modified in order to detect the entity as a fan rather than as a light.
 
 **This is a spin-off from the original Home Assistant integration which was marked as deprecated and will be removed in Home Assistant Core 2022.4.**
 
-The rpi_gpio_pwm platform allows to control multiple lights using pulse-width modulation, for example LED strips. It supports one-color LEDs driven by GPIOs of a Raspberry Pi (same host or remote)
+The rpi_fan_gpio_pwm platform allows to control multiple fans using pulse-width modulation. It supports FANs driven by GPIOs of a Raspberry Pi (same host or remote)
 For controlling the GPIOs, the platform connects to the pigpio-daemon (http://abyz.me.uk/rpi/pigpio/pigpiod.html), which must be running. On Raspbian Jessie 2016-05-10 or newer the pigpio library is already included. On other operating systems it needs to be installed first (see installation instructions: https://github.com/soldag/python-pwmled#installation).
 
 For Home Assistant this daemon can be installed as an add-on (https://github.com/Poeschl/Hassio-Addons/tree/master/pigpio).
 
-# Installation
+## Installation
 
 ### HACS
 
-The recommend way to install `ha-rpi_gpio_pwm` is through [HACS](https://hacs.xyz/).
+The recommend way to install `ha-rpi_fan_gpio_pwm` is through [HACS](https://hacs.xyz/).
 
 ### Manual installation
 
-Copy the `ha-rpi_gpio_pwm` folder and all of its contents into your Home Assistant's `custom_components` folder. This folder is usually inside your `/config` folder. If you are running Hass.io, use SAMBA to copy the folder over. You may need to create the `custom_components` folder and then copy the `ha-rpi_gpio_pwm` folder and all of its contents into it.
+Copy the `ha-rpi_fan_gpio_pwm` folder and all of its contents into your Home Assistant's `custom_components` folder. This folder is usually inside your `/config` folder. If you are running Hass.io, use SAMBA to copy the folder over. You may need to create the `custom_components` folder and then copy the `ha-rpi_fan_gpio_pwm` folder and all of its contents into it.
 
-# Configuration
+## Configuration
 To enable this platform, add the following lines to your configuration.yaml:
 
 ```yaml
 # Example configuration.yaml entry
-light:
+fan:
   - platform: rpi_gpio_pwm
-    leds:
+    fans:
       - name: Lightstrip Cupboard
         pin: 17
 ```
-# CONFIGURATION VARIABLES
-- **leds** list *(REQUIRED)*: Can contain multiple LEDs.
 
-- **name** string *(REQUIRED)*: The name of the LED.
+## CONFIGURATION VARIABLES
+- **fans** list *(REQUIRED)*: Can contain multiple FANs.
 
-- **pin** integer *(REQUIRED)*: The pin connected to the LED as a list.
+- **name** string *(REQUIRED)*: The name of the FAN.
 
-- **frequency** integer *(optional, default: 100)*: The PWM frequency.
+- **pin** integer *(REQUIRED)*: The pin connected to the FAN as a list.
 
 - **host** string *(optional, default: localhost)*: The remote host address for the GPIO driver.
 
 - **port** integer *(optional, default: 8888)*: The port on which the GPIO driver is listening.
 
+## Authors & contributors
+
+This project is a fork of the original [RedMeKool](https://github.com/RedMeKool/HA-Raspberry-pi-GPIO-PWM) project.
+
+## License
+
+MIT License
+
+Copyright (c) 2023 [Frosh](https://github.com/erdnaxela02)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+>
+> **_BECAUSE IT IS A FORK, Parts of the project are originally Copyright (c) 2022, [RedMeKool](https://github.com/RedMeKool/HA-Raspberry-pi-GPIO-PWM)_**
+>
